c13c0e6ebf9ed578c166551f48ab291f34fa3c00
Priyanka-0712 <priyankaboke801@gmail.com>
2026-02-09 17:48:24 +0530

    list is added

---
+++ ArrayListDemo/src/com/aaraylist/Arraylist.java
@@ -0,0 +1,13 @@
+package com.aaraylist;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+public static void main(String[] args) {
+	ArrayList list=new ArrayList();
+	list.add(10);
+	list.add(20);
+	list.add(30);
+	System.out.println(list);
+}
+}
