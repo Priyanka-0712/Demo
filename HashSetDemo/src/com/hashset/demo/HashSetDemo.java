ad711d661141a2b3fb1f23440d6ce5dd59a4dfd5
Priyanka-0712 <priyankaboke801@gmail.com>
2026-02-09 22:46:56 +0530

    names added into set

---
+++ HashSetDemo/src/com/hashset/demo/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.hashset.demo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+public static void main(String[] args) {
+	HashSet set=new HashSet();
+	set.add("priyanka");
+	set.add("renuka");
+	set.add("prasad");
+	set.add("avinash");
+	set.add("rekha");
+	System.out.println(set);
+}
+}
